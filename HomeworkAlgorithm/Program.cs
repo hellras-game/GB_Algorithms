f886d243d62ce2e2190c80b0f9162a9e4498a33a
hellras-game <72730151+hellras-game@users.noreply.github.com>
2022-05-24 02:51:34 +1100

    Add files via upload

---
+++ HomeworkAlgorithm/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HomeworkAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void l2Task1()
+            {
+                
+            }
+            int taskNumb()
+            {
+                Console.WriteLine("Choose task number (1 or 3)");
+                int a = Convert.ToInt32(Console.ReadLine());
+                return a;
+            }
+
+            switch (taskNumb())
+            {
+                case 1: l1Task1();
+                    break;
+                case 3: l1Task3();
+                    break;
+                default: Console.WriteLine("Wrong task number");
+                    break;
+            }
+
+            void l1Task1()
+            {
+                Console.WriteLine("Enter an integer");
+                long number = Convert.ToInt64(Console.ReadLine());
+                Task1L1.check(number);
+            }
+
+            void l1Task3()
+            {
+                Console.WriteLine("Enter the index of the Fibonacci number");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Task2L1.recFibonacci(n);
+                Task2L1.cycleFibonacci(n);
+            }
+
+        }
+
+    }
+}
---
+++ HomeworkAlgorithm/Task1L2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkAlgorithm
+{
+    public interface ILinkedList
+    {
+        int GetCount(); // возвращает количество элементов в списке
+        void AddNode(int value); // добавляет новый элемент списка
+        void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+        void RemoveNode(int index); // удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); // удаляет указанный элемент
+        Node FindNode(int searchValue); // ищет элемент по его значению
+    }
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public int count { get; set; }
+
+        public LinkedList(int value)
+        {
+            Node item = new Node(value);
+            Head = item;
+            Tail = item;
+            count = 1;
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public void AddNode(int value)
+        {
+            Node item = new Node(value);
+            Tail.NextNode = item;
+            item.PrevNode = Tail;
+            Tail = item;
+            count++;
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            Node item = new Node(value);
+            Node current = Head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(node))
+                {
+                    current.NextNode.PrevNode = item;
+                    current.NextNode = item;
+                    count++;
+                }
+            }
+        }
+        public void RemoveNode(int index)
+        {
+
+            int currentIndex = 0;
+            Node currentNode = Head;
+            while (currentNode != null)
+            {
+                if (currentIndex == index - 1)
+                {
+                    currentNode.NextNode = currentNode.NextNode.NextNode;
+                    currentNode.NextNode.NextNode.PrevNode = currentNode;
+                    count--;
+                    return;
+                }
+                currentNode = currentNode.NextNode;
+                currentIndex++;
+            }
+
+        }
+        public void RemoveNode(Node node)
+        {
+            Node current = Head;
+
+            while (current != null)
+            {
+                if(current.Value.Equals(node))
+                {
+                    current.PrevNode.NextNode = current.NextNode;
+                    current.NextNode.PrevNode = current.PrevNode;
+                    count--;
+                    return;
+                }
+            }
+
+            current = current.NextNode;
+        }
+        public Node FindNode(int searchValue)
+        {
+            Node currentNode = Head;
+            while (currentNode != null)
+            {
+                if (currentNode.Value == searchValue)
+                    return currentNode;
+                currentNode = currentNode.NextNode;
+            }
+            return null;
+        }
+    }
+}
